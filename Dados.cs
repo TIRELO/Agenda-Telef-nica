57115b2e7022c4270ac5c7fd13d045eab5d0ccaf
TIRELO <40547307+TIRELO@users.noreply.github.com>
2020-10-04 13:22:05 -0300

    Create Dados.cs

---
+++ Dados.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaTelefonica
+{
+    public class Dados
+    {
+        public string nome;
+        public int numero;
+        public string email;
+    }
+}
